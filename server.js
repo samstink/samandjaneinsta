4faa9542ede1822ca642775e8be5cc32765bf030
samstink <samfairbairn@stinkdigital.com>
2015-05-29 14:45:40 +0100

    hide vine

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Instagram = require('instagram-node-lib');
 var Twitter = require('twitter');
 var http = require('http');
 var request = ('request');
-var vine = require('node-vine');
+//var vine = require('node-vine');
 var intervalID;
 
 /**
@@ -38,6 +38,22 @@ var client = new Twitter({
     access_token_key: "44387693-4TNHyJ5elWZGLbuRWiWqnlOH9AxjLatC3Mhnb2MsL",
     access_token_secret: "X98bB3c0XN3ygEHUYWz9UTkt5do5DEexQXVaJSHpWMrxZ"
 });
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {track: 'samandjane'},  function(stream){
+
+    stream.on('data', function(tweet) {
+        console.log(tweet.text);
+    });
+
+    stream.on('error', function(error) {
+        console.log(error);
+    });
+});
+
 
 /**
  * Uses the library "instagram-node-lib" to Subscribe to the Instagram API Real Time
@@ -141,22 +157,6 @@ io.sockets.on('connection', function (socket) {
         });
 
     });*/
-
-    /**
-     * Stream statuses filtered by keyword
-     * number of tweets per second depends on topic popularity
-     **/
-    client.stream('statuses/filter', {track: 'samandjane'},  function(stream){
-
-        stream.on('data', function(tweet) {
-            console.log(tweet.text);
-        });
-
-        stream.on('error', function(error) {
-            console.log(error);
-        });
-    });
-
 });
 
 /**
